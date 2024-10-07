0a1bb7d1ed1af4914e09a72d92498669f8abf16c
Oskar <oskar.janusz@teb.edu.pl>
2024-10-07 10:27:29 +0200

    ppp

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
+from account.forms import LoginForm
 from django.shortcuts import render
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = LoginForm()
+    return render(request,'account/login.html',{'form':form})
