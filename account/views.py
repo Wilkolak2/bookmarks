5841f58cad3f29b6c32dbba7916aa111371727d7
student15 <111@111>
2024-10-21 10:28:32 +0200

    ppp

--- account/views.py
+++ account/views.py
@@ -1,9 +1,31 @@
-from account.forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from account.forms import LoginForm
+
+"""
 def user_login(request):
     if request.method == 'POST':
-        pass
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            user = authenticate(username=cleaned_data['username'],
+                                password=cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnianie zakończyło się sukcesem')
+                else:
+                    return HttpResponse('Konto jest zablokowane')
+            else:
+                return HttpResponse('Nieprawidłowe dane logowania')
     else:
         form = LoginForm()
-    return render(request,'account/login.html',{'form':form})
+    return render(request, 'account/login.html', {'form': form})
+"""
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)r$pmkl7@+b&7q=j&ks3l%#*xxt(zhfag^eaz!d@kp6gxwci-u'
+SECRET_KEY = 'django-insecure-zzme^8uzk=!p9&xr5&2xwy!cqvl0b3_(f9dm-c+8f6k3n#1p8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
