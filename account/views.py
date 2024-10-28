bd9053c401c525eea89404106479998144baedd4
student15 <111@111>
2024-10-28 10:28:44 +0100

    28.10

--- account/views.py
+++ account/views.py
@@ -32,7 +32,12 @@ def dashboard(request):
 
 def register(request):
     if request.method == 'POST':
-        pass
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request,
